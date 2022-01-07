f148bf1787bcab2b7571729c963f16cff06526eb
holee <holee@emoney.co.kr>
2022-01-07 20:20:22 +0900

    feat: MVC Pattern - Servlet

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.servletmvc;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String viewPath = "/WEB-INF/views/new-form.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+		
+		// 다른 서블릿이나 JSP로 이동할 수 있는 기능
+		// 클라이언트가 서버 호출 -> 서버 내부적으로 다시 호출 발생
+		dispatcher.forward(request,response);
+
+	}
+}
